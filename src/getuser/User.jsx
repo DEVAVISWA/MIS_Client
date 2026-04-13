0a982a90beb77c2062bf2d57ef06f4994fb26324
DEVAVISWA <devaviswa15@gmail.com>
2026-04-13 16:59:44 +0530

    axios installed

--- src/getuser/User.jsx
+++ src/getuser/User.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./user.css";
+import axios from "axios";
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.log("err while fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="userTable">
-        <button type="button" class="btn btn-primary">Add User</button>
+      <button type="button" className="btn btn-primary">
+        Add User<i className="fa-solid fa-user-plus"></i>
+      </button>
 
       <table className="table table-bordered">
         <thead>
@@ -17,13 +32,25 @@ const User = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Deva</td>
-            <td>deva@ccb.bank.in</td>
-            <td>rathiri 10 maniki</td>
-            <td>update | delete</td>
-          </tr>
+          {users.map((user, index) => {
+            return (
+              <tr className="actionButtons">
+                <td>{index + 1}</td>
+                <td> {user.name} </td>
+                <td> {user.email}</td>
+                <td>{user.message}</td>
+                <td>
+                  <button type="button" className="btn btn-info">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-danger">
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
